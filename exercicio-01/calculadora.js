4b051362fd61f5ff7a83118218a1b4e42f456b2b
devporpino <isaacgithub14@gmail.com>
2025-02-28 18:22:42 -0300

    Primeiro commit

---
+++ exercicio-01/calculadora.js
@@ -0,0 +1,15 @@
+const x = parseFloat(prompt("Informe um número: "))
+const y = parseFloat(prompt("Informe outro número: "))
+
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert(
+    "Resultado: \n" + 
+    "\nSoma: " + soma + 
+    "\nSubtração: " + subtracao + 
+    "\nmultiplicação: " + multiplicacao + 
+    "\nDivisão: " + divisao
+)
---
+++ exercicio-01/index.js
@@ -0,0 +1,12 @@
+const nome = prompt("Digite o seu nome: ")
+const sobreNome = prompt("Digite seu sobrenome: ")
+const campoDeestudo = prompt("Informe seu campo de estudo: ")
+const nasciMento = prompt("Informe sua data de nascimento: ")
+
+alert(
+   " Resultado: \n" +
+    "\nnome: " + nome + 
+    "\nSobrenome: " + sobreNome + 
+    "\nCampo de estudo: " + campoDeestudo + 
+    "\nData de nascimento: " + (2024 - nasciMento )
+)
